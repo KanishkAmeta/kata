5c4abfdd6cd1c5740733237a7d8ef3ee55ad0e12
Kanishk Ameta <Kanishk Ameta@LAPTOP-D6IFURHR>
2020-05-26 21:25:12 +0530

    Commit1 red

---
+++ StringCalculator/src/StringCalculator.java
@@ -0,0 +1,9 @@
+
+public class StringCalculator {
+
+	public static String sum(String input) {
+		return "-1";
+
+	}
+
+}
---
+++ StringCalculator/src/StringCalculatorTest.java
@@ -0,0 +1,11 @@
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StringCalculatorTest {
+@Test
+public void Stringempty() {
+	Assert.assertEquals("0",StringCalculator.sum(""));
+}
+
+}
