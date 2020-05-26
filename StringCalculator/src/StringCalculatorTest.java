7fc1d439c0d04c55fa1c302a8fc02bf7df63fcdd
Kanishk Ameta <Kanishk Ameta@LAPTOP-D6IFURHR>
2020-05-27 00:24:48 +0530

    Three test cases passed in a string

--- StringCalculator/src/StringCalculatorTest.java
+++ StringCalculator/src/StringCalculatorTest.java
@@ -17,4 +17,10 @@ public void twonumber() {
 	Assert.assertEquals(4,StringCalculator.Add("1,3"));
 	Assert.assertEquals(2,StringCalculator.Add("1,1"));
 }
+@Test
+public void threenumbers() {
+	Assert.assertEquals(6,StringCalculator.Add("1,2,3"));
+	Assert.assertEquals(3,StringCalculator.Add("1,1,1"));
+	Assert.assertEquals(1,StringCalculator.Add("1,0,0"));
+}
 }
